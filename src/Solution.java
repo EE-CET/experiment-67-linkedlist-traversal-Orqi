5936074877c2baf1147dc53abeb50f841cacc0fc
Amrita Reji <amritareji@Amritas-MacBook-Pro.local>
2026-02-28 21:35:19 +0530

    hi

--- src/Solution.java
+++ src/Solution.java
@@ -18,14 +18,20 @@ public class Solution {
 
         System.out.print("Forward: ");
         while (iterator.hasNext()) {
-            System.out.print(iterator.next() + " ");
+            System.out.print(iterator.next());
+            if (iterator.hasNext()) {
+                System.out.print(" ");
+            }
         }
 
         System.out.println();
 
         System.out.print("Backward: ");
         while (iterator.hasPrevious()) {
-            System.out.print(iterator.previous() + " ");
+            System.out.print(iterator.previous());
+            if (iterator.hasPrevious()) {
+                System.out.print(" ");
+            }
         }
 
         scanner.close();
